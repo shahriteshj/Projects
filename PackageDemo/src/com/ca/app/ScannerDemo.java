32696f1b1f090daaffc3bdd8441ad46bff8b735a
shahriteshj <43956811+shahriteshj@users.noreply.github.com>
2018-10-16 16:22:42 +0530

    .

---
+++ PackageDemo/src/com/ca/app/ScannerDemo.java
@@ -0,0 +1,17 @@
+package com.ca.app;
+
+import java.util.Scanner;
+
+public class ScannerDemo {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt();
+
+        System.out.println("Hello "+ name+ " you are "+age);
+    }
+}
