ade889224e93f90ebd10840ccbfbf2c291ca093c
shahriteshj <43956811+shahriteshj@users.noreply.github.com>
2018-10-19 17:45:38 +0530

    .

--- PackageDemo/src/com/ca/app/GenericStudent.java
+++ PackageDemo/src/com/ca/app/GenericStudent.java
@@ -1,10 +1,14 @@
 package com.ca.app;
 
 public class GenericStudent<T> {
+    private int age;
+    private int id;
+    private String firstName;
+    private String lastName;
 
     public int compareTo(T t) {
         System.out.println("Using compareTo!!");
-        return this.rollNo - o.rollNo;
+        return this.id - t.id;
     }
 
 }
