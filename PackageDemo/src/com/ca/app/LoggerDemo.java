b4e7a4eb8756aed26a4cb69c8c05772e3b85fa9e
shahriteshj <43956811+shahriteshj@users.noreply.github.com>
2018-10-22 18:12:57 +0530

    .

--- PackageDemo/src/com/ca/app/LoggerDemo.java
+++ PackageDemo/src/com/ca/app/LoggerDemo.java
@@ -1,9 +1,18 @@
 package com.ca.app;
 
-import org.apache.logging.log4j.core.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class LoggerDemo {
+
+    static Logger log = LogManager.getLogger(LoggerDemo.class);
+
     public static void main(String[] args) {
+        log.info("Info Message");
+        log.debug("Debug Message");
+        log.error("Error Message");
+        log.fatal("Exception Occurred", new RuntimeException("Dummy"));
+
 
     }
 
