486126bd701f479ee69241dac931c5915195d0bf
shahriteshj <43956811+shahriteshj@users.noreply.github.com>
2018-10-25 12:59:50 +0530

    .

---
+++ PackageDemo/src/com/ca/app/ThreadDemo.java
@@ -0,0 +1,40 @@
+package com.ca.app;
+
+public class ThreadDemo implements Runnable{
+    private String statement;
+
+    public ThreadDemo(String statement) {
+        this.statement = statement;
+    }
+
+    @Override
+    public void run() {
+        for(int i=0;i<statement.length();i++){
+            System.out.println(Thread.currentThread().getName());
+            System.out.println(" "+ statement.charAt(i));
+            Thread.yield();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        //Create TASK
+        ThreadDemo d = new ThreadDemo("The quick brown fox jumps over the lazy dog.");
+        ThreadDemo d2 = new ThreadDemo("123456789124564564645464");
+
+        //Create WORKER thread and assign task
+        Thread t1 = new Thread(d);
+        Thread t2 = new Thread(d2);
+
+        //Launch THREAD
+        t1.start();
+        t2.start();
+
+        try {
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("End of program");
+    }
+}
