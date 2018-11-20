8000e72933250f1ac1fe0ea6c1240a38fd06c5c2
shahriteshj <43956811+shahriteshj@users.noreply.github.com>
2018-11-20 15:33:57 +0530

    .

--- AngularJS/MyShoppingProject/src/app/app-routing.module.ts
+++ AngularJS/MyShoppingProject/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { EmployeeInfoComponent } from './employee-info/employee-info.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { EmplistserviceComponent } from './emplistservice/emplistservice.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'employeeslist', component: EmployeeInfoComponent },
+  { path: 'employees', component: EmployeeComponent },
+  { path: 'employeeservice', component: EmplistserviceComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AngularJS/MyShoppingProject/src/app/app.module.ts
+++ AngularJS/MyShoppingProject/src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AdminModule } from '../admin/admin.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { EmpPipe } from './pipe/emp.pipe';
+import { EmployeeInfoComponent } from './employee-info/employee-info.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { EmpdataService } from './services/empdata.service';
+import { EmplistserviceComponent } from './emplistservice/emplistservice.component';
+import { AdminRoutingModule } from 'src/admin/admin-routing.module';
+
 
 @NgModule({
   declarations: [
@@ -14,13 +21,17 @@ import { EmpPipe } from './pipe/emp.pipe';
     HomeComponent,
     EmployeeComponent,
     ContactUsComponent,
-    EmpPipe
+    EmpPipe,
+    EmployeeInfoComponent,
+    PageNotFoundComponent,
+    EmplistserviceComponent
   ],
   imports: [
     BrowserModule,
+    AdminModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [EmpdataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
