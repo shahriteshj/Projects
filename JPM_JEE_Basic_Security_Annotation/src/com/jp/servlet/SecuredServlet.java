92c6ccfb5b59ebd88f526d3ef0077ebf3c0de658
shahriteshj <43956811+shahriteshj@users.noreply.github.com>
2018-12-07 16:13:17 +0530

    .

---
+++ JPM_JEE_Basic_Security_Annotation/src/com/jp/servlet/SecuredServlet.java
@@ -0,0 +1,34 @@
+package com.jp.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.HttpConstraint;
+import javax.servlet.annotation.HttpMethodConstraint;
+import javax.servlet.annotation.ServletSecurity;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SecuredServlet
+ */
+@WebServlet("/SecuredServlet")
+@ServletSecurity(
+		value=@HttpConstraint(rolesAllowed={"tomcat"}),
+		httpMethodConstraints = {
+				@HttpMethodConstraint(value="GET",rolesAllowed={"tomcat"}),
+				@HttpMethodConstraint(value="POST",rolesAllowed={"tomcat"})
+		})
+public class SecuredServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
