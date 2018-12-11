16174b3d8bd44a0d280d4d1b462d3fa8e820a8a6
shahriteshj <43956811+shahriteshj@users.noreply.github.com>
2018-12-11 11:09:41 +0530

    .

--- Spring040_lifecycle/src/main/java/com/jp/hr/beans/ServiceBean.java
+++ Spring040_lifecycle/src/main/java/com/jp/hr/beans/ServiceBean.java
@@ -1,17 +1,90 @@
 package com.jp.hr.beans;
 
-public class ServiceBean {
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+
+/*
+ * The implementation of InitializingBean interface
+ * 		* The name of implementing method cannot be customizable.
+ *  	* The exception being thrown cannot be changed.
+ *  	* The initializing code cannot be grouped as per category in different methods.
+ * 		* The interface tightly bound code to spring framework API. 
+ */
+
+@Component("service")
+public class ServiceBean implements InitializingBean, ApplicationContextAware {
 	
 	private int x;
+	private ApplicationContext ctx;
 	
-	public ServiceBean(int x){
-		this.x = x;
-		System.out.println("In constructor.");
+	public ServiceBean(){
+		System.out.println("In Constructor.");
 	}
 	
-	public void setValueX(int x){
+	@Autowired
+	public ServiceBean(@Value("10") int x){
 		this.x = x;
-		System.out.println("In setter.");
+		System.out.println("In constructor. " + this.x);
+	}
+	
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.ctx = applicationContext;
+		System.out.println("Setting Application Context.");
+		
+	}
+	
+	@Value("20")
+	public void setValueX( int x){
+		this.x = x;
+		System.out.println("In setter. " + this.x);
+	}
+	
+	@PostConstruct
+	public void validateConfigX(){
+		System.out.println("In validateConfigX().");
+		if(x>100){
+			System.out.println("Wrong input.");
+		}
+	}
+	
+	@PostConstruct
+	public void validateConfigZ(){
+		System.out.println("In validateConfigZ().");
+		if(x>100){
+			System.out.println("Wrong input.");
+		}
+	}
+	
+	@PostConstruct
+	public void validateConfigY(){
+		System.out.println("In validateConfigY().");
+		if(x>100){
+			System.out.println("Wrong input.");
+		}
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("In afterPropertiesSet().");
+		
+	}
+
+	@Override
+	public String toString() {
+		return "ServiceBean [x=" + x + "]";
+	}
+
+	public void myServices(){
+		ctx.getBean("");
 	}
 
 }
---
+++ Spring040_lifecycle/src/main/java/com/jp/hr/tests/TestLifeCycle.java
@@ -0,0 +1,18 @@
+package com.jp.hr.tests;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.jp.hr.beans.ServiceBean;
+
+public class TestLifeCycle {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new  ClassPathXmlApplicationContext("context.xml");
+
+		ServiceBean bean = (ServiceBean) ctx.getBean("service");
+		System.out.println(bean);
+		
+	}
+
+}
