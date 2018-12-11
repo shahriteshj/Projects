83dcfacf9ab58bc823d561a5b237750b1bb4b7d8
shahriteshj <43956811+shahriteshj@users.noreply.github.com>
2018-12-11 16:57:27 +0530

    .

--- Spring100_MVCBasics/src/com/jp/hr/controllers/HomePageController.java
+++ Spring100_MVCBasics/src/com/jp/hr/controllers/HomePageController.java
@@ -6,9 +6,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomePageController {
 
-	@RequestMapping("/homePage")
+	@RequestMapping("/")
 	public String getHomePage(){
 		System.out.println("In getHomePage().");
 		return "/HomePage.jsp";
 	}
+	
+	@RequestMapping("/homePage")
+	public String getHomePage1(){
+		System.out.println("In getHomePage().");
+		return "/HomePage.jsp";
+	}
+	
 }
